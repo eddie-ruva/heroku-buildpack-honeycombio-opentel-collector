8f85318ee5745c4a7818e4cf33393ec53af27600
Eddie Ruvalcaba <eddie@useascend.com>
2023-07-09 19:44:07 -0700

    Download open telemetry collector from honeycomb

--- extra/otelcol.sh
+++ extra/otelcol.sh
@@ -12,5 +12,7 @@ fi
 if [ -n "$DISABLE_OTELCOL" ]; then
   echo "The OpenTelemetry Collector agent has been disabled. Unset the $DISABLE_OTELCOL or set missing environment variables."
 else
+  echo "-----> Starting OpenTelemetry Collector"
   bash -c "otelcol --config $APP_OTELCOL/config.yml 2>&1 &"
+  echo "-----> Started OpenTelemetry Collector"
 fi
