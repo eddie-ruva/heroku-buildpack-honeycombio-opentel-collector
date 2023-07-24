ba66cd0c0b56dda015c6f1a05342dc3ba892e180
Eddie Ruvalcaba <eddie@useascend.com>
2023-07-23 22:52:20 -0700

    Push config

--- extra/otelcol-runner.sh
+++ extra/otelcol-runner.sh
@@ -7,15 +7,24 @@ if [ "$DYNOTYPE" == "run" ]; then
   exit 0
 fi
 
+export CUSTOM_APP_CONFIG="$APP_OTELCOL/config.yaml"
+
+if [[ -f "$CUSTOM_APP_CONFIG" ]]; then
+  export OTEL_CONFIG="${CUSTOM_APP_CONFIG}"
+else
+  export OTEL_CONFIG="${OTELCOL_CONFIG_DIR}/config.yaml"
+fi
+
 if [ -n "$DISABLE_OTELCOL" ]; then
   echo "The OpenTelemetry Collector agent has been disabled. Unset the $DISABLE_OTELCOL or set missing environment variables."
 else
   echo "-----> Starting OpenTelemetry Collector"
   echo $(ls)
   echo $(ls $OTELCOL_CONFIG_DIR)
-  echo "$OTELCOL_CONFIG_DIR/otelcol --config $APP_OTELCOL/config.yml 2>&1 &"
+  echo "USING CONFIG: ${OTEL_CONFIG}"
+  echo "$OTELCOL_CONFIG_DIR/otelcol --config $OTEL_CONFIG 2>&1 &"
   echo "-----> "
   chmod a+x $OTELCOL_CONFIG_DIR/otelcol_hny_linux_amd64
-  $OTELCOL_CONFIG_DIR/otelcol_hny_linux_amd64 --config $APP_OTELCOL/config.yml 2>&1&
+  $OTELCOL_CONFIG_DIR/otelcol_hny_linux_amd64 --config $OTEL_CONFIG 2>&1&
   echo "-----> Started OpenTelemetry Collector"
 fi
